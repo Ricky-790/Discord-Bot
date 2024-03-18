316a2777ba4ef73694383e192c0f6933e4f11546
Snehel Basu <137712318+Ricky-790@users.noreply.github.com>
2024-03-18 12:13:37 +0530

    Basic Bot done

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+import os
+import gemini_api as gp
+
+
+#Secret key
+TOKEN = os.environ.get('DISCORD_TOKEN')
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+        if message.content == ('Hello'):
+            await message.channel.send('Hii!')
+        #Check if we are tagged
+        if self.user in message.mentions:
+            gp.convo.send_message(message.content)
+            await message.channel.send(gp.convo.last.text)
+        
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run(TOKEN)
+
+
