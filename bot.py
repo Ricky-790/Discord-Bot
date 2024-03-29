0af074dd0c314bfdab49e5ad387aabd62bcd99c7
Ricky <www.rickyjordan@gmail.com>
2024-03-29 11:04:36 +0530

    none

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
 import discord
 import os
 import gemini_api as gp
-
-
 #Secret key
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print(self.user+' is Online!')
 
     async def on_message(self, message):
         # don't respond to ourselves
@@ -20,11 +18,8 @@ class MyClient(discord.Client):
         #Check if we are tagged
         if self.user in message.mentions:
             gp.convo.send_message(message.content)
-            await message.channel.send(gp.convo.last.text)
-        
+            await message.channel.send(gp.convo.last.text)        
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
 client.run(TOKEN)
-
-
